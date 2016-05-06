414832f8500f5c911cded2e42324d87e9c1cf09d
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-05-05 22:28:21 -0300

    Add search actions

---
+++ app/actions/search.js
@@ -0,0 +1,20 @@
+import { stringify } from 'qs'
+import { CALL_API } from '../middleware/api'
+
+export const SEARCH_REQUEST = 'SEARCH_REQUEST'
+export const SEARCH_SUCCESS = 'SEARCH_SUCCESS'
+export const SEARCH_FAILURE = 'SEARCH_FAILURE'
+
+export function search(term, type) {
+    const params = stringify({
+        q: term,
+        type
+    })
+
+    return {
+        [CALL_API]: {
+            types: [SEARCH_REQUEST, SEARCH_SUCCESS, SEARCH_FAILURE],
+            endpoint: `search?${params}`
+        }
+    }
+}
